2df270a8516ef04ea9f2e105a8819d4817d56621
GOPAL MANI DUBEY <deepakdubeygi@gmail.com>
2019-11-06 15:27:01 +0530

    Add files via upload

---
+++ geo.php
@@ -0,0 +1,26 @@
+<?php
+if (!empty($_GET['location'])) {
+    /**
+     * URL for google maps api
+     */
+    $maps_url = 'https://' .
+        'maps.googleapis.com/' .
+        'maps/api/geocode/json' .
+        '?address=' . urlencode($_GET['location']);
+    $maps_json = file_get_contents($maps_url);
+    $maps_array = json_decode($maps_json, true);
+    $lat = $maps_array['results'][0]['geometry']['location']['lat'];
+    $lng = $maps_array['results'][0]['geometry']['location']['lng'];
+    /**
+     * we will make Instagram api request. We'll build the url using the
+     * coordinate values returned by the google maps api
+     */
+    $url = 'https://' .
+        'api.instagram.com/v1/media/search' .
+        '?lat=' . $lat .
+        '&lng=' . $lng .
+        '&client_id=CLIENT-ID'; //replace "CLIENT-ID" to your id
+    $json = file_get_contents($url);
+    $array = json_decode($json, true);
+}
+?>
